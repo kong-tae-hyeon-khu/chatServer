78ccd1a2d8c0b60342e3d928d191cba375ddcf46
kong-tae-hyeon-khu <kong3047@khu.ac.kr>
2023-02-12 13:33:10 +0900

    Install socket.io & construct socket

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = express();
 
 const PORT = 8000;
 
+const { Server } = require("socket.io");
+const path = "/socket.io"; // 통신을 수행할 경로.
+// 서버의 정보와 통신 경로를 넘겨준다.
+const io = new Server(app, { path: path });
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
